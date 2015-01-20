519406d1870640250a133d1113f5d410ae6fd9c0
jiayuanmark <jiayuanmark@gmail.com>
2015-01-19 22:30:37 -0800

    performance improvement

--- hackrank-fp-practice/passwordCracker.hs
+++ hackrank-fp-practice/passwordCracker.hs
@@ -10,51 +10,46 @@ import qualified Data.Set as S
 import Debug.Trace (trace)
 
 type Dict  = S.Set C.ByteString
-type Table = IOUArray (Int,Int) Bool
+type Table = IOUArray Int Int
 
 -- substring between i and j
 substr :: Int -> Int -> C.ByteString -> C.ByteString
 substr i j = C.drop (i-1) . (C.take j)
 
-crack :: Dict -> C.ByteString -> Int -> IO Table
-crack dict pwd !ln = do
-  arr <- newArray bnds False :: IO Table
-  forM_ [ (i,i+l) | l <- [0..(n-1)], i <- [1..n], i+l <= n ] (dp arr)
+crack :: Dict -> C.ByteString -> [Int] -> IO Table
+crack dict pwd lns = do
+  arr <- newArray bnds (-1) :: IO Table
+  forM_ [0..n] (dp arr)
   return arr
   where
-    match i j  = (substr i j pwd) `S.member` dict
+    match i j = (substr i j pwd) `S.member` dict
 
-    n          = C.length pwd
-    bnds       = ((1,1),(n,n))
+    n         = C.length pwd
+    bnds      = (0,n)
     
-    dp arr t@(i,j)
-      | match i j  = writeArray arr t True
-      | otherwise  = go arr i j (i+ln-1) >>= writeArray arr t
+    dp arr 0  = writeArray arr 0 (-2)
+    dp arr i  = go arr i lns >>= writeArray arr i
 
-    go arr i j k
-      | k >= j     = return False
-      | otherwise  = do 
-        lft <- readArray arr (i,k)
-        rht <- readArray arr (k+1,j)
-        case (lft && rht) of
-          True -> return True
-          _    -> go arr i j $! (k+1)
+    go arr i [] = return (-1)
+    go arr i (l:ls)
+      | (i-l) < 0 = go arr i ls
+      | otherwise = do
+        sub <- readArray arr (i-l)
+        if (sub /= -1) && (match (i-l+1) i)
+        then return (i-l) 
+        else go arr i ls
 
-backTrack :: Dict -> Table -> C.ByteString -> Int -> Int -> IO ()
-backTrack dc tb w !ofs n
-  | match w   = putStr " " >> C.putStrLn w
-  | otherwise = do
-    pts <- filterM (pick tb) [1..(C.length w)]
-    let (u,v) = C.splitAt (head pts) w
-    case ofs of
-      0 -> C.putStr u 
-      _ -> putStr " " >> C.putStr u 
-    backTrack dc tb v (ofs + (head pts)) n
-  where
-    match w    = w `S.member` dc
-    pick arr i = case (match (C.take i w)) of
-        False -> return False
-        _     -> readArray arr (ofs+i+1,n) 
+backTrack :: Table -> C.ByteString -> Int -> Int -> IO ()
+backTrack tb w cur n = do
+  prev <- readArray tb cur
+  if prev < 0
+  then return ()
+  else do
+    backTrack tb w prev n
+    C.putStr (substr (prev+1) cur w)
+    if cur == n 
+    then putStrLn ""
+    else putStr " "
 
 solve :: Int -> IO ()
 solve 0 = return ()
@@ -62,14 +57,14 @@ solve t = do
   _   <- (read <$> getLine) :: IO Int
   wds <- map C.pack . words <$> getLine
   pwd <- C.pack <$> getLine
-  let n = C.length pwd
-  let ln  = L.foldl1' min (map C.length wds)
   let dic = S.fromList wds
-  tab <- crack dic pwd ln
-  ans <- readArray tab (1,n)
+  let ls  = map C.length wds
+  tab <- crack dic pwd ls
+  let n   = C.length pwd
+  ans <- readArray tab n
   case ans of
-    False -> putStrLn "WRONG PASSWORD"
-    True  -> backTrack dic tab pwd 0 n
+    -1 -> putStrLn "WRONG PASSWORD"
+    _  -> backTrack tab pwd n n
   solve $! (t-1)
 
 main :: IO ()
--- hackrank-fp-practice/passwordCrackerLazy.hs
+++ hackrank-fp-practice/passwordCrackerLazy.hs
@@ -9,38 +9,33 @@ import qualified Data.Set as S
 import Debug.Trace (trace)
 
 type Dict  = S.Set C.ByteString
-type Table = Array (Int,Int) Bool
+type Table = Array Int Int
 
 -- substring between i and j
 substr :: Int -> Int -> C.ByteString -> C.ByteString
 substr i j = C.drop (i-1) . (C.take j)
 
-crack :: Dict -> C.ByteString -> Table
-crack dict pwd = table
+crack :: Dict -> C.ByteString -> [Int] -> Table
+crack dict pwd lns = table
   where
-    match i j  = (substr i j pwd) `S.member` dict
+    match i j = (substr i j pwd) `S.member` dict
 
-    n          = C.length pwd
-    bnds       = ((1,1),(n,n))
+    n         = C.length pwd
+    bnds      = (0,n)
 
-    table     :: Table
-    table      = array bnds [ (ij, dp ij) | ij <- range bnds ]
+    table    :: Table
+    table     = array bnds [ (i, dp i) | i <- range bnds ]
     
-    dp (i,j)
-      | i > j      = False
-      | i == j     = match i j
-      | match i j  = True
-      | otherwise  = any (\k -> (table ! (i,k)) && (table ! (k+1,j))) [i..(j-1)]
+    dp 0      = -2
+    dp i      = let p = filter (check i) lns
+                in if L.null p then -1 else i - (head p)
+    check i l = and [(i-l >= 0), (table ! (i-l) /= -1), (match (i-l+1) i)]
 
-backTrack :: Dict -> Table -> C.ByteString -> Int -> Int -> [C.ByteString]
-backTrack dc tb w ofs n
-  | pass w    = [w]
-  | otherwise = u:(backTrack dc tb v (ofs+sp) n)
-  where
-    pass   = flip S.member dc
-    pick i = (pass (C.take i w)) && (tb ! (ofs+i+1,n)) 
-    sp     = head $ filter pick [1..]
-    (u,v)  = C.splitAt sp w
+backTrack :: Table -> C.ByteString -> Int -> [C.ByteString]
+backTrack tb w cur
+  | prev < 0  = []
+  | otherwise = (backTrack tb w prev) ++ [ substr (prev+1) cur w ]
+  where prev = tb ! cur
 
 solve :: Int -> IO ()
 solve 0 = return ()
@@ -50,17 +45,14 @@ solve t = do
   pwd <- C.pack <$> getLine
   let n = C.length pwd
   let dic = S.fromList wds
-  let tab = crack dic pwd
-  case tab ! (1,n) of
-    False -> putStrLn "WRONG PASSWORD"
-    True  -> C.putStrLn $ C.intercalate (C.pack " ") (backTrack dic tab pwd 0 n)
+  let lns = map C.length wds
+  let tab = crack dic pwd lns
+  case tab ! n of
+    -1 -> putStrLn "WRONG PASSWORD"
+    _  -> C.putStrLn $ C.intercalate (C.pack " ") (backTrack tab pwd n)
   solve $! (t-1)
 
 main :: IO ()
 main = do
   t <- read <$> getLine
   solve t
-
-
-
-
