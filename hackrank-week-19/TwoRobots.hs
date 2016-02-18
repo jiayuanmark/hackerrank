270b3134bb5c859709fb8f7949b19432b257c15f
jiayuanmark <jiayuanmark@gmail.com>
2016-02-18 08:39:31 -0800

    inefficient impl of two robots

--- hackrank-week-19/TwoRobots.hs
+++ hackrank-week-19/TwoRobots.hs
@@ -1,5 +1,10 @@
+{-# LANGUAGE BangPatterns #-}
+
 import Control.Applicative
 import Control.Monad
+import Data.Array.Unboxed
+
+type Query = [(Int,Int)]
 
 solve :: IO ()
 solve = do
@@ -9,8 +14,28 @@ solve = do
     return (x, y)
   putStrLn . show $ calc m query
 
-calc :: Int -> [(Int,Int)] -> Int
-calc = undefined
+calc :: Int -> Query -> Int
+calc _ []         = 0
+calc m ((a,b):qs) = calc' edge qs
+  where
+    inf  = 2 * (10 ^ 6) :: Int
+    bnds = ((1,1),(m,m))
+    edge :: UArray (Int, Int) Int
+    edge = array bnds [ (ij, fill ij) | ij <- range bnds ]
+      where fill (x,y)
+              | x == b || y == b = abs (a - b)
+              | otherwise        = inf
+    calc' :: UArray (Int,Int) Int -> Query -> Int
+    calc' !arr []        = minimum [ arr ! ij | ij <- range bnds ]
+    calc' !arr ((x,y):q) = calc' arr' q
+      where arr' = array bnds [ (ij, dp ij) | ij <- range bnds ]
+            val  = abs (x - y)
+            dp (u,v)
+              | u == y    = minimum [ abs (x - u') + val + arr ! (u',v)
+                                      | u' <- [1..m] ]
+              | v == y    = minimum [ abs (x - v') + val + arr ! (u,v')
+                                      | v' <- [1..m] ]
+              | otherwise = inf
 
 main :: IO ()
 main = do
