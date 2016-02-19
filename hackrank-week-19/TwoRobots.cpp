e4f39af0f53622d5723cedb75fe3a041b09a51f6
jiayuanmark <jiayuanmark@gmail.com>
2016-02-18 23:34:34 -0800

    fix cpp problem

--- hackrank-week-19/TwoRobots.cpp
+++ hackrank-week-19/TwoRobots.cpp
@@ -1,58 +1,86 @@
 #include <cstdio>
 #include <cstring>
-#include <cmath>
 
 using namespace std;
 
 static const int INF = 2 * 1e6;
 static const int N = 1005;
-int table[N][N];
+int table[N][N], copy[N][N];
 int x[N], y[N];
 int m, n;
 
-void init(const int& m) {
-    for (int i = 0; i <= m; ++i) {
-        for (int j = 0; j <= m; ++j) {
-            table[i][j] = INF;
-        }
-    }
+inline int ABS(const int& x) {
+  return x > 0 ? x : -x;
 }
 
-int sol(const int& m) {
-    int res = INF;
-    for (int i = 1; i <= m; ++i) {
-        for (int j = 1; j <= m; ++j) {
-            res = min(res, INF);
-        }
+inline int MIN(const int& x, const int& y) {
+  return x < y ? x : y;
+}
+
+void init() {
+  int val = ABS(y[0] - x[0]);
+  for (int i = 0; i <= m; ++i) {
+    for (int j = 0; j <= m; ++j) {
+      if (i == y[0] || j == y[0]) {
+        table[i][j] = val;
+      } else {
+        table[i][j] = INF;
+      }
     }
-    return res;
+  }
+}
+
+int sol() {
+  int res = INF;
+  for (int i = 1; i <= m; ++i) {
+    for (int j = 1; j <= m; ++j) {
+      res = MIN(res, table[i][j]);
+    }
+  }
+  return res;
+}
+
+void transfer() {
+  for (int i = 1; i <= m; ++i) {
+    memcpy(table[i], copy[i], sizeof(copy[i]));
+  }
 }
 
 int main() {
-    int t;
-    scanf("%d", &t);
-    while (t--) {
-        scanf("%d %d", &m, &n);
-        for (int i = 0; i < n; ++i) {
-            scanf("%d %d", &x[i], &y[i]);
-        }
-
-        init(m);
-        for (int i = 1; i <= m; ++i) {
-            table[i][y[0]] = table[y[0]][i] = abs(x[0] - y[0]);
-        }
-
-        for (int i = 1; i < n; ++i) {
-            for (int j = 1; j <= m; ++j) {
-                for (int k = 1; k <= m; ++k) {
-                    if (j == y[i]) {
-                        table[j][k] = mi1;
-                    } else if (k == y[i])
-                }
-            }
-        }
-
-
+  int t;
+  scanf("%d", &t);
+  while (t--) {
+    scanf("%d %d", &m, &n);
+    for (int i = 0; i < n; ++i) {
+      scanf("%d %d", &x[i], &y[i]);
     }
-    return 0;
+
+    init();
+    for (int i = 1; i < n; ++i) {
+      // dp
+      int u = ABS(x[i] - y[i]);
+      for (int j = 1; j <= m; ++j) {
+        for (int k = 1; k <= m; ++k) {
+          if (j == y[i]) {
+            int v = INF;
+            for (int l = 1; l <= m; ++l) {
+              v = MIN(v, table[l][k] + ABS(l - x[i]) + u);
+            }
+            copy[j][k] = v;
+          } else if (k == y[i]) {
+            int v = INF;
+            for (int l = 1; l <= m; ++l) {
+              v = MIN(v, table[j][l] + ABS(l - x[i]) + u);
+            }
+            copy[j][k] = v;
+          } else {
+            copy[j][k] = INF;
+          }
+        }
+      }
+      transfer();
+    }
+    printf("%d\n", sol());
+  }
+  return 0;
 }
