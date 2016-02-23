a6a92aa241374c9877227037bcac3f4e828f2dd4
jiayuanmark <jiayuanmark@gmail.com>
2016-02-23 00:33:09 -0800

    performance optimization

--- hackrank-week-19/ScalarProducts.hs
+++ hackrank-week-19/ScalarProducts.hs
@@ -1,8 +1,10 @@
 import Control.Applicative
 import qualified Data.Set as S
 
-fib :: [Integer]
-fib = 0 : 1 : zipWith (+) fib (tail fib)
+genFib :: Integer -> [Integer]
+genFib m = let f x y = (x + y) `mod` m
+               fib   = 0 : 1 : zipWith f fib (tail fib)
+           in fib
 
 solve :: Integer -> Integer -> Integer -> Int
 solve _ _ 1 = 0
@@ -10,7 +12,7 @@ solve c m n = S.size set
   where
     c'   = (c ^ 2) `mod` m
     n'   = fromIntegral n
-    tup  = gen . drop 2 . take (2 * n' + 2) $ fib
+    tup  = gen . drop 2 . take (2 * n' + 2) $ genFib m
     set  = S.fromList $ l1 ++ l2
     l1   = map (\x -> x * c' `mod` m) $ zipWith prod tup (tail tup)
     l2   = map (\x -> (prod x x) * c' `mod` m) $ init . tail $ tup
